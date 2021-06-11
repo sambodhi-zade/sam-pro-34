c8418e6c1be6d889893e1a15157ff74269cb5885
sambodhi-zade <82253573+sambodhi-zade@users.noreply.github.com>
2021-06-11 16:25:19 +0530

    Update mon.js

--- mon.js
+++ mon.js
@@ -3,13 +3,13 @@ class mon{
       var options ={ 
         isStatic: false,
       restitution:0.5,
-    friction:1.0
+    friction:1.0   
   }
       this.body =  Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
      
-      this.image=loadImage("images/Monster-01.png");
+      this.image=loadImage("Monster-01.png");
       
 
       World.add(world,this.body);
